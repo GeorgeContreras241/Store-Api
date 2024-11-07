0f91d498c44c3ab8afc093eaf318c1f68c5ee55d
GeorgeContreras241 <maicolcontreras241@gmail.com>
2024-11-06 21:39:30 -0500

    Se corrigieron los errores al desplegar y detalles css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,13 @@
-
 import Link from "next/link";
-
+import { Suspense } from "react";
+import Loading from "./products/loading";
 
 export default function Home() {
   return (
-    <>
+    <Suspense fallback={<Loading></Loading>}>
       <Link className="comenzar" href='/products' style={{"margin-top": "200px"}}>Comenzar</Link>
       
-    </>
+    </Suspense>
 
   )
 }
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -7,9 +7,10 @@ import { useState } from "react"
 import { FaSun } from "react-icons/fa";
 import { FaMoon } from "react-icons/fa";
 
+
 export default function Products() {
     const [darkMode, setDarkMode] = useState(false)
-    
+
     const handleMode = () => {
         if (darkMode) {
             document.body.classList.remove('darkMode')
@@ -19,18 +20,18 @@ export default function Products() {
             setDarkMode(true)
         }
     }
-     
+
     return (
         <main className={styles.products}>
             <article className={styles.filters}>
                 <Filter></Filter>
             </article>
-            <section className={styles.product}>
-                <Product></Product>
-            </section>
-            <Cart></Cart> 
-            <button className={styles.button__darkMode} 
-            onClick={handleMode}>{darkMode?<FaSun />:<FaMoon />}</button>
+                <section className={styles.product}>
+                    <Product></Product>
+                </section>
+            <Cart></Cart>
+            <button className={styles.button__darkMode}
+                onClick={handleMode}>{darkMode ? <FaSun /> : <FaMoon />}</button>
         </main>
 
 
